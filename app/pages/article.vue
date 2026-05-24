badc1e3d5fc4200b819ecee7381849f1c568d8ac
yunliyo <yunli.poetic.lee@gmail.com>
2026-05-24 20:06:54 +0800

    del bug

--- app/pages/article.vue
+++ app/pages/article.vue
@@ -2,9 +2,9 @@
 useHead({ title: '文章' })
 definePageMeta({ headerText: '最近更新' })
 
-const { data, error, status } = await useFetch('/api/feed/blog')
+const { data, error, status } = useLazyFetch('/api/feed/blog')
 
-const articles = computed(() => data.value?.slice(0, 20) ?? [])
+const articles = computed(() => data.value.slice(0, 11))
 </script>
 
 <template>
